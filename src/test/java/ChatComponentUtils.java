77193c25c80cbb32efa12341efd73826cbd19594
qu4ks <egorlapatskij@gmail.com>
2023-03-05 19:06:09 +0300

    ChatComponentUtils - comments

--- src/test/java/ChatComponentUtils.java
+++ src/test/java/ChatComponentUtils.java
@@ -6,23 +6,29 @@ import java.util.regex.Pattern;
 
 public class ChatComponentUtils {
 
+    // Replaces placeholders in a message with TextComponents
     public static TextComponent replacePlaceholders(String message, Map<String, TextComponent> placeholders) {
         TextComponent textComponent = new TextComponent();
 
+        // Use a regular expression to match placeholders in the message
         Pattern pattern = Pattern.compile("%(.*?)%");
         Matcher matcher = pattern.matcher(message);
         int lastIndex = 0;
 
+        // Iterate through the matches and replace them with TextComponents
         while (matcher.find()) {
             String placeholder = matcher.group(1);
             TextComponent replacement = placeholders.getOrDefault(placeholder, new TextComponent());
             if (matcher.start() > lastIndex) {
+                // Add any non-matching text to the final TextComponent
                 textComponent.addExtra(message.substring(lastIndex, matcher.start()));
             }
+            // Add the replacement TextComponent to the final TextComponent
             textComponent.addExtra(replacement);
             lastIndex = matcher.end();
         }
 
+        // Add any remaining non-matching text to the final TextComponent
         if (lastIndex < message.length()) {
             textComponent.addExtra(message.substring(lastIndex));
         }
@@ -38,7 +44,9 @@ public class ChatComponentUtils {
         placeholders.put("chat-type", new TextComponent("global"));
         placeholders.put("message", new TextComponent("Hello world!"));
 
+        // Replace the placeholders in the message with TextComponents
         TextComponent textComponent = replacePlaceholders(message, placeholders);
+        // Print the resulting TextComponent to the console
         System.out.println(textComponent.toLegacyText());
     }
 
